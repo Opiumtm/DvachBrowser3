f985ba71d4d42a09d33f0662ec1df88994b0ab43
boethiah <opium_tm@live.ru>
2015-02-22 01:39:19 +0300

    1

---
+++ DvachBrowser3.Core/Common/CoreConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace DvachBrowser3
+{
+    /// <summary>
+    /// Основные константы.
+    /// </summary>
+    public static class CoreConstants
+    {
+         
+    }
+}
---
+++ DvachBrowser3.Core/Common/NetworkProfile.cs
@@ -0,0 +1,33 @@
+﻿namespace DvachBrowser3
+{
+    /// <summary>
+    /// Профиль использования сети.
+    /// </summary>
+    public enum NetworkProfile : int
+    {
+        /// <summary>
+        /// Экстремальный режим.
+        /// </summary>
+        Extremal = 4,
+
+        /// <summary>
+        /// Подключение по WiFi.
+        /// </summary>
+        WiFi = 3,
+
+        /// <summary>
+        /// Хороший 3G.
+        /// </summary>
+        Good3G = 2,
+
+        /// <summary>
+        /// Плохой 3G.
+        /// </summary>
+        Bad3G = 1,
+
+        /// <summary>
+        /// Экономия трафика.
+        /// </summary>
+        Economic = 0
+    }
+}
