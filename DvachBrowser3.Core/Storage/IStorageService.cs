4708e46d24ab4aa2420ca0226cb60f37aaf13eae
boethiah <opium_tm@live.ru>
2015-04-24 21:22:19 +0300

    Внутреннее хранилище

--- DvachBrowser3.Core/Storage/IStorageService.cs
+++ DvachBrowser3.Core/Storage/IStorageService.cs
@@ -5,34 +5,5 @@
     /// </summary>
     public interface IStorageService
     {
-        /// <summary>
-        /// Тред.
-        /// </summary>
-        IThreadStorageService Thread { get; }
-
-        /// <summary>
-        /// Борды.
-        /// </summary>
-        IBoardStorageService Board { get; set; }
-
-        /// <summary>
-        /// Постинг.
-        /// </summary>
-        IPostingStorageService Posting { get; }
-
-        /// <summary>
-        /// Файлы постинга.
-        /// </summary>
-        IPostingFilesStorageService PostingFiles { get; }
-
-        /// <summary>
-        /// Медиа файлы.
-        /// </summary>
-        IMediaStorageService Media { get; set; }
-
-        /// <summary>
-        /// Предпросмотр.
-        /// </summary>
-        IMediaStorageService ThumbnailMedia { get; set; }
     }
 }
