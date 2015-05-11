978b6b22562a38961ff371f29b2636fd97f909e7
boethiah <opium_tm@live.ru>
2015-05-11 22:03:19 +0300

    Базовый класс модели представления страницы

---
+++ DvachBrowser3.ViewModels/IPageStateAware.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace DvachBrowser3
+{
+    /// <summary>
+    /// Объект, поддерживающий сохранение состояния.
+    /// </summary>
+    public interface IPageStateAware
+    {
+        /// <summary>
+        /// Загрузить состояние.
+        /// </summary>
+        /// <param name="navigationParameter">Параметр навигации.</param>
+        /// <param name="pageState">Состояние страницы (null - нет состояния).</param>
+        void OnLoadState(object navigationParameter, Dictionary<string, object> pageState);
+
+        /// <summary>
+        /// Сохранить состояние.
+        /// </summary>
+        /// <param name="pageState">Состояние страницы.</param>
+        void OnSaveState(Dictionary<string, object> pageState);
+    }
+}
---
+++ DvachBrowser3.ViewModels/PageViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace DvachBrowser3
+{
+    /// <summary>
+    /// Базовый класс модели представления страницы.
+    /// </summary>
+    public abstract class PageViewModelBase : ObservableObject, IPageStateAware
+    {
+        /// <summary>
+        /// Загрузить состояние.
+        /// </summary>
+        /// <param name="navigationParameter">Параметр навигации.</param>
+        /// <param name="pageState">Состояние страницы (null - нет состояния).</param>
+        public virtual void OnLoadState(object navigationParameter, Dictionary<string, object> pageState)
+        {
+        }
+
+        /// <summary>
+        /// Сохранить состояние.
+        /// </summary>
+        /// <param name="pageState">Состояние страницы.</param>
+        public virtual void OnSaveState(Dictionary<string, object> pageState)
+        {            
+        }
+    }
+}
