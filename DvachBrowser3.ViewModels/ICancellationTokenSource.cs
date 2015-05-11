68f7cbb1e81fd1f76bfacd94ab16ae294a8ea6f2
boethiah <opium_tm@live.ru>
2015-05-12 02:49:19 +0300

    Модели представления

---
+++ DvachBrowser3.ViewModels/ICancellationTokenSource.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+
+namespace DvachBrowser3
+{
+    /// <summary>
+    /// Источник токена отмены.
+    /// </summary>
+    public interface ICancellationTokenSource
+    {
+        /// <summary>
+        /// Получить токен отмены.
+        /// </summary>
+        /// <returns>Токен отмены.</returns>
+        CancellationToken GetToken();
+    }
+}
--- DvachBrowser3.ViewModels/PageViewModelBase.cs
+++ DvachBrowser3.ViewModels/PageViewModelBase.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Threading;
 
 namespace DvachBrowser3
 {
@@ -22,6 +23,47 @@ namespace DvachBrowser3
         /// <param name="pageState">Состояние страницы.</param>
         public virtual void OnSaveState(Dictionary<string, object> pageState)
         {            
+        }
+    }
+
+    /// <summary>
+    /// Базовый класс модели представления страницы с возможностью отмены.
+    /// </summary>
+    public abstract class CancellablePageViewMovelBase : PageViewModelBase, ICancellationTokenSource
+    {
+        /// <summary>
+        /// Источник токенов отмены.
+        /// </summary>
+        protected CancellationTokenSource TokenSource { get; set; }
+
+        /// <summary>
+        /// Получить токен отмены.
+        /// </summary>
+        /// <returns>Токен отмены.</returns>
+        public CancellationToken GetToken()
+        {
+            return TokenSource != null ? TokenSource.Token : new CancellationToken();
+        }
+
+        /// <summary>
+        /// Сохранить состояние.
+        /// </summary>
+        /// <param name="pageState">Состояние страницы.</param>
+        public override void OnSaveState(Dictionary<string, object> pageState)
+        {
+            base.OnSaveState(pageState);
+            if (TokenSource != null)
+            {
+                TokenSource.Cancel();
+                TokenSource.Dispose();
+                TokenSource = null;                
+            }
+        }
+
+        public override void OnLoadState(object navigationParameter, Dictionary<string, object> pageState)
+        {
+            TokenSource = new CancellationTokenSource();
+            base.OnLoadState(navigationParameter, pageState);
         }
     }
 }
