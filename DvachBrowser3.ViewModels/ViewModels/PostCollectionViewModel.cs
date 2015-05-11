dc6027f2bf5d3e7b965a72d2923d97d2c5705635
boethiah <opium_tm@live.ru>
2015-05-12 00:41:10 +0300

    Модели представления

--- DvachBrowser3.ViewModels/ViewModels/PostCollectionViewModel.cs
+++ DvachBrowser3.ViewModels/ViewModels/PostCollectionViewModel.cs
@@ -1,9 +1,19 @@
-﻿namespace DvachBrowser3.ViewModels
+﻿using DvachBrowser3.Links;
+
+namespace DvachBrowser3.ViewModels
 {
     /// <summary>
     /// Коллекция постов.
     /// </summary>
     public sealed class PostCollectionViewModel : ViewModelBase, IPostCollectionViewModel
-    {         
+    {
+        /// <summary>
+        /// Перейти к посту.
+        /// </summary>
+        /// <param name="link">Ссылка.</param>
+        public void GotoPost(BoardLinkBase link)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
