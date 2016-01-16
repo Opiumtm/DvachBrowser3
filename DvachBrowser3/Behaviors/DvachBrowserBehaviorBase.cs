916a8d6aa31a3968b8990acb87cbc41e28dcec9f
boethiah <opium_tm@live.ru>
2016-01-16 05:35:52 +0300

    Всплывающие меню

---
+++ DvachBrowser3/Behaviors/DvachBrowserBehaviorBase.cs
@@ -0,0 +1,61 @@
+﻿using Windows.UI.Xaml;
+using Microsoft.Xaml.Interactivity;
+
+namespace DvachBrowser3.Behaviors
+{
+    /// <summary>
+    /// Базовый класс поведения.
+    /// </summary>
+    public abstract class DvachBrowserBehaviorBase : DependencyObject, IBehavior
+    {
+        /// <summary>
+        /// Attaches to the specified object.
+        /// </summary>
+        /// <param name="associatedObject">The <see cref="T:Windows.UI.Xaml.DependencyObject"/> to which the <seealso cref="T:Microsoft.Xaml.Interactivity.IBehavior"/> will be attached.</param>
+        public void Attach(DependencyObject associatedObject)
+        {
+            AssociatedObject = associatedObject;
+            OnAttach(associatedObject);
+        }
+
+        /// <summary>
+        /// Detaches this instance from its associated object.
+        /// </summary>
+        public void Detach()
+        {
+            OnDetach(AssociatedObject);
+            AssociatedObject = null;
+        }
+
+        /// <summary>
+        /// Действие по присоединению к объекту.
+        /// </summary>
+        /// <param name="associatedObject">Объект.</param>
+        protected virtual void OnAttach(DependencyObject associatedObject)
+        {
+        }
+
+        /// <summary>
+        /// Действие по отсоединению от объекта.
+        /// </summary>
+        /// <param name="associatedObject">Объект.</param>
+        protected virtual void OnDetach(DependencyObject associatedObject)
+        {            
+        }
+
+        /// <summary>
+        /// Связанный объект.
+        /// </summary>
+        public DependencyObject AssociatedObject
+        {
+            get { return (DependencyObject)GetValue(AssociatedObjectProperty); }
+            set { SetValue(AssociatedObjectProperty, value); }
+        }
+
+        /// <summary>
+        /// Связанный объект.
+        /// </summary>
+        public static readonly DependencyProperty AssociatedObjectProperty = DependencyProperty.Register("AssociatedObject", typeof(DependencyObject), typeof(DvachBrowserBehaviorBase),
+            new PropertyMetadata(null));
+    }
+}
---
+++ DvachBrowser3/Behaviors/PopupMenuBehavior.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Windows.Foundation;
+using Windows.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Microsoft.Xaml.Interactivity;
+
+namespace DvachBrowser3.Behaviors
+{
+    /// <summary>
+    /// Поведение с выпадающим меню.
+    /// </summary>
+    [TypeConstraint(typeof(UIElement))]
+    public sealed class PopupMenuBehavior : DvachBrowserBehaviorBase
+    {
+        /// <summary>
+        /// Действие по присоединению к объекту.
+        /// </summary>
+        /// <param name="associatedObject">Объект.</param>
+        protected override void OnAttach(DependencyObject associatedObject)
+        {
+            var uiElement = associatedObject as UIElement;
+            if (uiElement != null)
+            {
+                uiElement.Holding += UiElementOnHolding;
+                uiElement.PointerPressed += UiElementOnPointerPressed;
+                uiElement.RightTapped += UiElementOnRightTapped;
+            }
+        }
+
+        /// <summary>
+        /// Действие по отсоединению от объекта.
+        /// </summary>
+        /// <param name="associatedObject">Объект.</param>
+        protected override void OnDetach(DependencyObject associatedObject)
+        {
+            var uiElement = associatedObject as UIElement;
+            if (uiElement != null)
+            {
+                uiElement.Holding -= UiElementOnHolding;
+                uiElement.PointerPressed -= UiElementOnPointerPressed;
+                uiElement.RightTapped -= UiElementOnRightTapped;
+            }
+        }
+
+        private void UiElementOnRightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            var uiElement = sender as UIElement;
+            if (isPointerPressed)
+            {
+                ShowMenu(e.GetPosition(uiElement));
+                e.Handled = true;
+            }
+        }
+
+        private void UiElementOnPointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            isPointerPressed = true;
+        }
+
+        private bool isPointerPressed;
+
+        private void UiElementOnHolding(object sender, HoldingRoutedEventArgs e)
+        {
+            var uiElement = sender as UIElement;
+            if (e.HoldingState == HoldingState.Completed && uiElement != null)
+            {
+                var position = e.GetPosition(uiElement);
+                ShowMenu(position);
+                e.Handled = true;
+                isPointerPressed = false;
+                uiElement?.CancelDirectManipulations();
+            }
+        }
+
+        private void ShowMenu(Point offset)
+        {
+            var uiElement = AssociatedObject as UIElement;
+            if (MenuFlyout != null && uiElement != null)
+            {
+                MenuFlyout.ShowAt(uiElement, offset);
+            }
+        }
+
+        /// <summary>
+        /// Меню.
+        /// </summary>
+        public MenuFlyout MenuFlyout
+        {
+            get { return (MenuFlyout) GetValue(MenuFlyoutProperty); }
+            set { SetValue(MenuFlyoutProperty, value); }
+        }
+
+        /// <summary>
+        /// Меню.
+        /// </summary>
+        public static readonly DependencyProperty MenuFlyoutProperty = DependencyProperty.Register("MenuFlyout", typeof (MenuFlyout), typeof (PopupMenuBehavior), new PropertyMetadata(null));         
+    }
+}
