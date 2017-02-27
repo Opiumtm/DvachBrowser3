88dc588f2b23eda5f49f222d8e4197ac01af4ce6
Opiumtm <opium_tm@live.ru>
2017-02-27 21:57:29 +0300

    Хранение данных модели

---
+++ DvachBrowser3.Ui.Common/ViewModels/IViewModelPersistService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DvachBrowser3.Ui.ViewModels
+{
+    /// <summary>
+    /// Сервис хранения данных моделей представления.
+    /// </summary>
+    public interface IViewModelPersistService
+    {
+        /// <summary>
+        /// Сохранить данные.
+        /// </summary>
+        /// <typeparam name="T">Тип данных.</typeparam>
+        /// <param name="key">Ключ.</param>
+        /// <param name="data">Данные.</param>
+        /// <param name="validUntil">Данные актуальны до указанной даты.</param>
+        /// <returns>Таск.</returns>
+        Task SaveData<T>(string key, T data, DateTime validUntil);
+
+        /// <summary>
+        /// Загрузить данные.
+        /// </summary>
+        /// <typeparam name="T">Тип данных.</typeparam>
+        /// <param name="key">Ключ.</param>
+        /// <param name="defaultValue">Значение по умолчанию.</param>
+        /// <returns>Таск.</returns>
+        Task<T> LoadData<T>(string key, T defaultValue = default(T));
+    }
+}
---
+++ DvachBrowser3.Ui.Common/ViewModels/PersistentViewModelBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+
+namespace DvachBrowser3.Ui.ViewModels
+{
+    /// <summary>
+    /// Модель представления, поддерживающая сохранение и восстановление данных.
+    /// </summary>
+    /// <typeparam name="TVisualState">Визуальное состояние.</typeparam>
+    public abstract class PersistentViewModelBase<TVisualState> : ViewModelBase<TVisualState> where TVisualState : VisualState
+    {
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="dispatcher">Диспетчер.</param>
+        protected PersistentViewModelBase(CoreDispatcher dispatcher) : base(dispatcher)
+        {
+        }
+
+        /// <summary>
+        /// Получить сервис хранения данных.
+        /// </summary>
+        /// <returns>Сервис хранения данных.</returns>
+        protected abstract Task<IViewModelPersistService> GetPersistService();
+
+        /// <summary>
+        /// Получить время, до которого данные считаются валидными.
+        /// </summary>
+        /// <returns>Время.</returns>
+        protected virtual DateTime GetPersistedStateValidPeriod()
+        {
+            return DateTime.Now.AddMinutes(30);
+        }
+    }
+}
